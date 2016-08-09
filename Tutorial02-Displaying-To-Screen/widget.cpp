37d35e35776b0de93597b00e80c4834f7ee2e43e
tommego <licaike@deepin.com>
2016-08-09 21:54:47 +0800

    Add Tutorial02-Displaying-To-Screen

--- Tutorial02-Displaying-To-Screen/widget.cpp
+++ Tutorial02-Displaying-To-Screen/widget.cpp
@@ -1,11 +1,33 @@
 #include "widget.h"
+extern "C"
+{
+#include <libavformat/avformat.h>
+#include <libavcodec/avcodec.h>
+#include <libavutil/pixfmt.h>
+#include <libswscale/swscale.h>
+}
 
 Widget::Widget(QWidget *parent)
     : QWidget(parent)
 {
+    m_label = new QLabel(this);
+    m_layout = new QVBoxLayout(this);
+    m_layout->addWidget(m_label);
+    setLayout(m_layout);
+    m_ffmpegWorker = new FFmpengWorker;
+    connect(FFmpengWorker::instance(),SIGNAL(displayScreenUpdate(QImage)),this,SLOT(onUpdateScreen(QImage)));
+    connect(FFmpengWorker::instance(),SIGNAL(displayFinished()),this,SLOT(displayFinished()));
+    FFmpengWorker::instance()->displayToScreen("/home/tommego/a.mp4");
+
 }
 
 Widget::~Widget()
 {
 
+}
+void Widget::onUpdateScreen(QImage img){
+    m_label->setPixmap(QPixmap::fromImage(img));
+}
+void Widget::displayFinished(){
+    qApp->quit();
 }
--- engine/ffmpengworker.cpp
+++ engine/ffmpengworker.cpp
@@ -168,141 +168,143 @@ QList<QPixmap>* FFmpengWorker::getScreenCaps(QString fileUrl){
 }
 
 void FFmpengWorker::displayToScreen(QString fileUrl){
-    // Initalizing these to NULL prevents segfaults!
-    AVFormatContext   *pFormatCtx = NULL;
-    int               i, videoStream;
-    AVCodecContext    *pCodecCtxOrig = NULL;
-    AVCodecContext    *pCodecCtx = NULL;
-    AVCodec           *pCodec = NULL;
-    AVFrame           *pFrame = NULL;
-    AVFrame           *pFrameRGB = NULL;
-    AVPacket          packet;
-    int               frameFinished;
-    int               numBytes;
-    uint8_t           *buffer = NULL;
-    struct SwsContext *sws_ctx = NULL;
-    QList<QPixmap>* capsList = new QList<QPixmap>();
+    QtConcurrent::run([=]{
+        // Initalizing these to NULL prevents segfaults!
+        AVFormatContext   *pFormatCtx = NULL;
+        int               i, videoStream;
+        AVCodecContext    *pCodecCtxOrig = NULL;
+        AVCodecContext    *pCodecCtx = NULL;
+        AVCodec           *pCodec = NULL;
+        AVFrame           *pFrame = NULL;
+        AVFrame           *pFrameRGB = NULL;
+        AVPacket          packet;
+        int               frameFinished;
+        int               numBytes;
+        uint8_t           *buffer = NULL;
+        struct SwsContext *sws_ctx = NULL;
 
-    // Register all formats and codecs
-    av_register_all();
+        // Register all formats and codecs
+        av_register_all();
 
-    // Open video file
-    if(avformat_open_input(&pFormatCtx, fileUrl.toStdString().data(), NULL, NULL)!=0)
-      return ; // Couldn't open file
+        // Open video file
+        if(avformat_open_input(&pFormatCtx, fileUrl.toStdString().data(), NULL, NULL)!=0)
+          return ; // Couldn't open file
 
-    // Retrieve stream information
-    if(avformat_find_stream_info(pFormatCtx, NULL)<0)
-      return ; // Couldn't find stream information
+        // Retrieve stream information
+        if(avformat_find_stream_info(pFormatCtx, NULL)<0)
+          return ; // Couldn't find stream information
 
-    // Dump information about file onto standard error
-    av_dump_format(pFormatCtx, 0, fileUrl.toStdString().data(), 0);
+        // Dump information about file onto standard error
+        av_dump_format(pFormatCtx, 0, fileUrl.toStdString().data(), 0);
 
-    // Find the first video stream
-    videoStream=-1;
-    for(i=0; i<pFormatCtx->nb_streams; i++)
-      if(pFormatCtx->streams[i]->codec->codec_type==AVMEDIA_TYPE_VIDEO) {
-        videoStream=i;
-        break;
-      }
-    if(videoStream==-1)
-      return ; // Didn't find a video stream
+        // Find the first video stream
+        videoStream=-1;
+        for(i=0; i<pFormatCtx->nb_streams; i++)
+          if(pFormatCtx->streams[i]->codec->codec_type==AVMEDIA_TYPE_VIDEO) {
+            videoStream=i;
+            break;
+          }
+        if(videoStream==-1)
+          return ; // Didn't find a video stream
 
-    // Get a pointer to the codec context for the video stream
-    pCodecCtxOrig=pFormatCtx->streams[videoStream]->codec;
-    // Find the decoder for the video stream
-    pCodec=avcodec_find_decoder(pCodecCtxOrig->codec_id);
-    if(pCodec==NULL) {
-      fprintf(stderr, "Unsupported codec!\n");
-      return ; // Codec not found
-    }
-    // Copy context
-    pCodecCtx = avcodec_alloc_context3(pCodec);
-    if(avcodec_copy_context(pCodecCtx, pCodecCtxOrig) != 0) {
-      fprintf(stderr, "Couldn't copy codec context");
-      return ; // Error copying codec context
-    }
-
-    // Open codec
-    if(avcodec_open2(pCodecCtx, pCodec, NULL)<0)
-      return ; // Could not open codec
-
-    // Allocate video frame
-    pFrame=av_frame_alloc();
-
-    // Allocate an AVFrame structure
-    pFrameRGB=av_frame_alloc();
-    if(pFrameRGB==NULL)
-      return capsList;
-
-    // Determine required buffer size and allocate buffer
-    numBytes=avpicture_get_size(AVPixelFormat::AV_PIX_FMT_RGB24, pCodecCtx->width,
-                    pCodecCtx->height);
-    buffer=(uint8_t *)av_malloc(numBytes*sizeof(uint8_t));
-
-    // Assign appropriate parts of buffer to image planes in pFrameRGBframeRGB
-    // Note that pFrameRGB is an AVFrame, but AVFrame is a superset
-    // of AVPicture
-    avpicture_fill((AVPicture *)pFrameRGB, buffer, AVPixelFormat::AV_PIX_FMT_RGB24,
-           pCodecCtx->width, pCodecCtx->height);
-
-    // initialize SWS context for software scaling
-    sws_ctx = sws_getContext(pCodecCtx->width,
-                 pCodecCtx->height,
-                 pCodecCtx->pix_fmt,
-                 pCodecCtx->width,
-                 pCodecCtx->height,
-                 AVPixelFormat::AV_PIX_FMT_RGB24,
-                 SWS_BILINEAR,
-                 NULL,
-                 NULL,
-                 NULL
-                 );
-
-    // Read frames and save first five frames to disk
-    i=0;
-    while(av_read_frame(pFormatCtx, &packet)>=0) {
-      // Is this a packet from the video stream?
-      if(packet.stream_index==videoStream) {
-        // Decode video frame
-        avcodec_decode_video2(pCodecCtx, pFrame, &frameFinished, &packet);
-
-        // Did we get a video frame?
-        if(frameFinished) {
-      // Convert the image from its native format to RGB
-
-            sws_scale(sws_ctx, (uint8_t const * const *)pFrame->data,
-                      pFrame->linesize, 0, pCodecCtx->height,
-                      pFrameRGB->data, pFrameRGB->linesize);
-            // Get the pixmap to screen
-            QImage img(pCodecCtx->width,pCodecCtx->height,QImage::Format_RGB888);
-            for( int y = 0; y < img.height(); ++y )
-               memcpy( img.scanLine(y), pFrameRGB->data[0]+y * pFrameRGB->linesize[0], img.width() * 3 );
-            //scale it smoothly
-            img = img.scaled(QSize(256,256),Qt::KeepAspectRatio,Qt::SmoothTransformation);
-
-            if(*m_displayPixmap)
-                m_displayPixmap = new QPixmap(QSize(256,256));
-            *m_displayPixmap = QPixmap::fromImage(img);
-            displayScreenUpdate(m_displayPixmap);
+        // Get a pointer to the codec context for the video stream
+        pCodecCtxOrig=pFormatCtx->streams[videoStream]->codec;
+        // Find the decoder for the video stream
+        pCodec=avcodec_find_decoder(pCodecCtxOrig->codec_id);
+        if(pCodec==NULL) {
+          fprintf(stderr, "Unsupported codec!\n");
+          return ; // Codec not found
         }
-      }
+        // Copy context
+        pCodecCtx = avcodec_alloc_context3(pCodec);
+        if(avcodec_copy_context(pCodecCtx, pCodecCtxOrig) != 0) {
+          fprintf(stderr, "Couldn't copy codec context");
+          return ; // Error copying codec context
+        }
 
-      // Free the packet that was allocated by av_read_frame
-      av_free_packet(&packet);
-    }
+        // Open codec
+        if(avcodec_open2(pCodecCtx, pCodec, NULL)<0)
+          return ; // Could not open codec
 
-    // Free the RGB image
-    av_free(buffer);
-    av_frame_free(&pFrameRGB);
+        // Allocate video frame
+        pFrame=av_frame_alloc();
 
-    // Free the YUV frame
-    av_frame_free(&pFrame);
+        // Allocate an AVFrame structure
+        pFrameRGB=av_frame_alloc();
+        if(pFrameRGB==NULL)
+          return ;
 
-    // Close the codecs
-    avcodec_close(pCodecCtx);
-    avcodec_close(pCodecCtxOrig);
+        // Determine required buffer size and allocate buffer
+        numBytes=avpicture_get_size(AVPixelFormat::AV_PIX_FMT_RGB24, pCodecCtx->width,
+                        pCodecCtx->height);
+        buffer=(uint8_t *)av_malloc(numBytes*sizeof(uint8_t));
 
-    // Close the video file
-    avformat_close_input(&pFormatCtx);
-    qDebug()<<"Capture screens completed";
+        // Assign appropriate parts of buffer to image planes in pFrameRGBframeRGB
+        // Note that pFrameRGB is an AVFrame, but AVFrame is a superset
+        // of AVPicture
+        avpicture_fill((AVPicture *)pFrameRGB, buffer, AVPixelFormat::AV_PIX_FMT_RGB24,
+               pCodecCtx->width, pCodecCtx->height);
+
+        // initialize SWS context for software scaling
+        sws_ctx = sws_getContext(pCodecCtx->width,
+                     pCodecCtx->height,
+                     pCodecCtx->pix_fmt,
+                     pCodecCtx->width,
+                     pCodecCtx->height,
+                     AVPixelFormat::AV_PIX_FMT_RGB24,
+                     SWS_BILINEAR,
+                     NULL,
+                     NULL,
+                     NULL
+                     );
+
+        // Read frames and save first five frames to disk
+        i=0;
+        while(av_read_frame(pFormatCtx, &packet)>=0) {
+          // Is this a packet from the video stream?
+          if(packet.stream_index==videoStream) {
+            // Decode video frame
+            avcodec_decode_video2(pCodecCtx, pFrame, &frameFinished, &packet);
+
+            // Did we get a video frame?
+            if(frameFinished) {
+          // Convert the image from its native format to RGB
+
+                qDebug()<<"scaling data:";
+                sws_scale(sws_ctx, (uint8_t const * const *)pFrame->data,
+                          pFrame->linesize, 0, pCodecCtx->height,
+                          pFrameRGB->data, pFrameRGB->linesize);
+                qDebug()<<"<<<<<<<<scaling completed";
+                // Get the pixmap to screen
+                QImage img(pCodecCtx->width,pCodecCtx->height,QImage::Format_RGB888);
+                for( int y = 0; y < img.height(); ++y )
+                   memcpy( img.scanLine(y), pFrameRGB->data[0]+y * pFrameRGB->linesize[0], img.width() * 3 );
+                //scale it smoothly
+                img = img.scaled(QSize(720,480),Qt::KeepAspectRatio,Qt::SmoothTransformation);
+
+                qDebug()<<"emitting signals";
+                emit displayScreenUpdate(img);
+            }
+          }
+
+          // Free the packet that was allocated by av_read_frame
+          av_free_packet(&packet);
+        }
+
+        // Free the RGB image
+        av_free(buffer);
+        av_frame_free(&pFrameRGB);
+
+        // Free the YUV frame
+        av_frame_free(&pFrame);
+
+        // Close the codecs
+        avcodec_close(pCodecCtx);
+        avcodec_close(pCodecCtxOrig);
+
+        // Close the video file
+        avformat_close_input(&pFormatCtx);
+        qDebug()<<"Capture screens completed";
+        emit displayFinished();
+    });
 }
